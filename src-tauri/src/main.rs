9d33eafca94209b65d23aadebe6cf3466d4ca88e
zzacal <jizacal@gmail.com>
2023-09-06 23:20:50 -0700

    Implements backend post call

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,6 +1,11 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use std::str::FromStr;
+
+use reqwest::{Client, header::{HeaderMap, HeaderValue, HeaderName}};
+use serde::{Deserialize, Serialize};
+
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
 fn greet(name: &str) -> String {
@@ -13,9 +18,10 @@ fn initialize() -> ChiConfig {
 }
 
 #[tauri::command]
-async fn send(request: ChiRequest) -> String {
+async fn send(request: Action) -> String {
     if request.method == "POST" {
-        send_post(request).await
+        println!("{:?}", request);
+        send_post(request).await.expect("some msg")
     } else if request.method == "PUT" {
         send_put(request).await
     } else {
@@ -23,22 +29,34 @@ async fn send(request: ChiRequest) -> String {
     }
 }
 
-async fn send_get(request: ChiRequest) -> String {
+async fn send_get(request: Action) -> String {
     let body = reqwest::get(request.url).await.expect("").text().await.expect("");
     body
 }
 
-async fn send_post(request: ChiRequest) -> String {
+
+async fn send_post(request: Action) -> Result<String, reqwest::Error> {
+    let client = Client::new();
+    
+    let response = client
+        .post(request.url)
+        .headers(map_to_header_map(request.headers))
+        .body(request.body)
+        .send()
+        .await?;
+    
+    let response_text = response.text().await?;
+    
+    Ok(response_text)
+}
+
+
+async fn send_put(request: Action) -> String {
     let body = reqwest::get(request.url).await.expect("").text().await.expect("");
     body
 }
 
-async fn send_put(request: ChiRequest) -> String {
-    let body = reqwest::get(request.url).await.expect("").text().await.expect("");
-    body
-}
-
-async fn send_delete(request: ChiRequest) -> String {
+async fn send_delete(request: Action) -> String {
     let body = reqwest::get(request.url).await.expect("").text().await.expect("");
     body
 }
@@ -50,7 +68,6 @@ fn main() {
         .expect("error while running tauri application");
 }
 
-use serde::{Deserialize, Serialize};
 #[derive(Serialize, Deserialize, Debug)]
 pub struct ChiRequest {
     method: String,
@@ -60,4 +77,40 @@ pub struct ChiRequest {
 #[derive(Serialize)]
 pub struct ChiConfig {
     methods: Vec<String>
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct EnabledKvp<TKey, TVal> {
+    is_enabled: bool,
+    key: TKey,
+    value: TVal,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Action {
+    method: String,
+    url: String,
+    name: String,
+    headers: Option<Vec<EnabledKvp<String, String>>>,
+    parameters: Option<Vec<EnabledKvp<String, String>>>,
+    body: String
+}
+
+
+fn map_to_header_map(headers: Option<Vec<EnabledKvp<String, String>>>) -> HeaderMap {
+    let mut header_map = HeaderMap::new();
+    
+    if let Some(enabled_kvps) = headers {
+        for kvp in enabled_kvps {
+            if kvp.is_enabled {
+                if let Ok(header_value) = HeaderValue::from_str(&kvp.value) {
+                    if let Ok(header_name) = HeaderName::from_str(&kvp.key) {
+                        header_map.insert(header_name, header_value);
+                    }
+                }
+            }
+        }
+    }
+    
+    header_map
 }
