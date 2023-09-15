c81b794a91b3f6dab3ec249014ed628084f05846
zzacal <jizacal@gmail.com>
2023-09-15 13:17:46 -0700

    Styles tabs

--- src/layout/Tabbable.svelte
+++ src/layout/Tabbable.svelte
@@ -1,7 +1,8 @@
 <script lang="ts">
   import Request from "../pages/library/Request.svelte";
   import type { Tab } from "./types";
-
+  import Close from "~icons/eva/close-fill";
+  import Save from "~icons/eva/save-outline";
   export let tabs: Tab[] = [];
   export let onCloseTab: (id: string) => void;
   export let open: Tab | null;
@@ -23,17 +24,25 @@
     }
   }
 </script>
-<ul class="tabs">
-  {#each tabs as tab }
-    <li class="tab {tab === open ? "active": ""}">
-      <a href="#{tab.label}" on:click={openTab(tab)}>
-        {tab.label}
-      </a>
-      <button on:click={remove(tab.id)}>X</button>
-  </li>
-  {/each}
-</ul>
-
+<nav class="actions">
+  <div class="menu">
+    <button on:click={() => console.log("imma save now")}>
+      <Save />
+    </button>
+  </div>
+  <ul class="tabs">
+    {#each tabs as tab }
+      <li class="tab {tab === open ? "active": ""}">
+        <a href="#{tab.label}" on:click={openTab(tab)}>
+          {tab.label}
+        </a>
+        <button class="close" on:click={remove(tab.id)}>
+          <Close />
+        </button>
+    </li>
+    {/each}
+  </ul>
+</nav>
 <div class="active">
   {#key open}
     {#if open?.type === "request"}
@@ -43,15 +52,44 @@
 </div>
 
 <style lang="scss">
-  .tabs {
-    list-style: none;
-    padding-left: unset;
+  nav {
+    display: flex;
+    align-items: center;
+
+    margin-top: .25rem;
     border-bottom: 1px solid var(--background-emphasis);
+
+    .menu {
+      padding: 0 .5rem;
+    }
+
+    button {
+      padding: unset;
+      margin: unset;
+      line-height: 1rem;
+      border-radius: .3rem;
+
+      &:focus, &:hover {
+        background-color: var(--button-background);
+        color: var(--button);
+      }
+    }
+  }
+  .tabs {
+    display: flex;
+    gap: .15rem;
+
+    margin: unset;
+    list-style: none;
+    padding: unset;
     .tab {
-      display: inline-block;
-      padding: .1rem .3rem;
+      display: flex;
+      gap: .5rem;
+
+      padding: .25rem .3rem;
       border-top-right-radius: .5rem;
       border-top-left-radius: .5rem;
+      border-right: 1px solid var(--background-preemph);
       
       &:hover {
         background-color: var(--background-preemph);
--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,16 @@
 import { defineConfig } from "vite";
 import { svelte } from "@sveltejs/vite-plugin-svelte";
+import Icons from 'unplugin-icons/vite';
 
 // https://vitejs.dev/config/
 export default defineConfig(async () => ({
-  plugins: [svelte()],
+  plugins: [
+    svelte(),
+    Icons({
+      compiler: 'svelte',
+      autoInstall: true,
+    }),
+  ],
 
   // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
   //
