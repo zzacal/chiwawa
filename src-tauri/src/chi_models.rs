d30f57078fd62df37ffc595e00adc4ff1de65579
zzacal <jizacal@gmail.com>
2023-09-14 18:09:04 -0700

    Modularizes chi rust types

---
+++ src-tauri/src/chi_models.rs
@@ -0,0 +1,81 @@
+use std::str::FromStr;
+
+use reqwest::header::{HeaderMap, HeaderValue, HeaderName};
+use serde::{Serialize, Deserialize};
+
+
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct ChiRequest {
+    pub method: String,
+    pub url: String,
+}
+
+#[derive(Serialize)]
+pub struct ChiConfig {
+    pub methods: Vec<String>
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct EnabledKvp<TKey, TVal> {
+    is_enabled: bool,
+    key: TKey,
+    value: TVal,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Action {
+    pub method: String,
+    pub url: String,
+    pub name: String,
+    pub headers: Option<Vec<EnabledKvp<String, String>>>,
+    pub parameters: Option<Vec<EnabledKvp<String, String>>>,
+    pub body: String
+}
+
+impl<TKey, TVal> EnabledKvp<TKey, TVal> {
+  pub fn try_kvp(&self) -> Option<(&TKey, &TVal)> {
+    if(self.is_enabled) {
+      Some((&self.key, &self.value))
+    } else {
+      None
+    }
+  }
+}
+
+pub fn map_to_headermap(headers: Vec<EnabledKvp<String, String>>) -> HeaderMap {
+  let mut header_map = HeaderMap::new();
+  let kvps = headers
+    .iter()
+    .map(|kvp| kvp.try_kvp())
+    .filter_map(|x| x)
+    .map(|(key, val)| (HeaderName::from_str(&key), HeaderValue::from_str(&val)));
+
+  for (key, value) in kvps {
+    if let Ok(name) = key {
+      if let Ok(val) = value {
+        header_map.insert(name, val);
+      }
+    }
+  }
+  
+  header_map
+}
+
+pub fn map_to_header_map(headers: Option<Vec<EnabledKvp<String, String>>>) -> HeaderMap {
+    let mut header_map = HeaderMap::new();
+    
+    if let Some(enabled_kvps) = headers {
+        for kvp in enabled_kvps {
+            if kvp.is_enabled {
+                if let Ok(header_value) = HeaderValue::from_str(&kvp.value) {
+                    if let Ok(header_name) = HeaderName::from_str(&kvp.key) {
+                        header_map.insert(header_name, header_value);
+                    }
+                }
+            }
+        }
+    }
+    
+    header_map
+}
--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -5,6 +5,8 @@ use std::str::FromStr;
 
 use reqwest::{Client, header::{HeaderMap, HeaderValue, HeaderName}};
 use serde::{Deserialize, Serialize};
+pub mod chi_models;
+use chi_models::{ChiConfig, Action, map_to_header_map};
 
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
@@ -153,52 +155,4 @@ impl From<Error> for SerializableError {
         
         SerializableError { message }
     }
-}
-
-
-#[derive(Serialize, Deserialize, Debug)]
-pub struct ChiRequest {
-    method: String,
-    url: String,
-}
-
-#[derive(Serialize)]
-pub struct ChiConfig {
-    methods: Vec<String>
-}
-
-#[derive(Serialize, Deserialize, Debug)]
-pub struct EnabledKvp<TKey, TVal> {
-    is_enabled: bool,
-    key: TKey,
-    value: TVal,
-}
-
-#[derive(Serialize, Deserialize, Debug)]
-pub struct Action {
-    method: String,
-    url: String,
-    name: String,
-    headers: Option<Vec<EnabledKvp<String, String>>>,
-    parameters: Option<Vec<EnabledKvp<String, String>>>,
-    body: String
-}
-
-
-fn map_to_header_map(headers: Option<Vec<EnabledKvp<String, String>>>) -> HeaderMap {
-    let mut header_map = HeaderMap::new();
-    
-    if let Some(enabled_kvps) = headers {
-        for kvp in enabled_kvps {
-            if kvp.is_enabled {
-                if let Ok(header_value) = HeaderValue::from_str(&kvp.value) {
-                    if let Ok(header_name) = HeaderName::from_str(&kvp.key) {
-                        header_map.insert(header_name, header_value);
-                    }
-                }
-            }
-        }
-    }
-    
-    header_map
 }
